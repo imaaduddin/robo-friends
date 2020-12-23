d11f3359f2b19582ead479ed4581e956953137a9
Imaad Uddin <imaaduddin@icloud.com>
2020-12-23 15:33:41 -0600

    worked on the index and card js files as well as robohash api

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div>
+      <img alt="robots" src="https://robohash.org/test?200x200" />
+      <div>
+        <h2>Robo1</h2>
+        <p>Robo1@autobots.com</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Card from './Card';
 import reportWebVitals from './reportWebVitals';
+import "tachyons";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <h1>Hello World!</h1>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Card />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
