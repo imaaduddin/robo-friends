d0e9b8a7cf207468da1b1d01f21775e49dcc9d00
Imaad Uddin <imaaduddin@icloud.com>
2020-12-28 17:10:43 -0600

    added new file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Card from './Card';
+import CardList from "./CardList";
 import reportWebVitals from './reportWebVitals';
 import "tachyons";
 import {robots} from "./robots";
