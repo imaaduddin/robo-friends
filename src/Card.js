e893642aa3cdc10113e3972f02106215fa1e3c17
Imaad Uddin <imaaduddin@icloud.com>
2020-12-28 16:44:38 -0600

    added the images

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-const Card = () => {
+const Card = (props) => {
   return (
-    <div>
-      <img alt="robots" src="https://robohash.org/test?200x200" />
+    <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+      <img alt="robots" src={`https://robohash.org/${props.id}?200x200`} />
       <div>
-        <h2>Robo1</h2>
-        <p>Robo1@autobots.com</p>
+        <h2>{props.name}</h2>
+        <p>{props.email}</p>
       </div>
     </div>
   )
