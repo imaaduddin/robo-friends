32fddd75f7a9fd1281bb41290779f18f7caf68c8
Imaad Uddin <imaaduddin@icloud.com>
2021-01-06 19:01:03 -0600

    worked on adding classes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
 import {robots} from "./robots";
 
 
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: robots,
+      searchfield: ""
+    }
+  }
 
-const App = () => {
-  return (
-    <div>
-      <h1>RoboFriends</h1>
-      <SearchBox />
-      <CardList robots={robots}/>
-    </div>
-  );
+  onSearchChange(event) {
+    console.log(event.target.value);
+  }
+
+  render() {
+    return (
+      <div className="tc">
+        <h1>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange}/>
+        <CardList robots={this.state.robots}/>
+      </div>
+    );
+  }
 }
 
 export default App;
